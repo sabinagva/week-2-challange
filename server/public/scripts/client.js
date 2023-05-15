b32fa32225f5a492d62a440446bb1a28c539fb4d
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-05-15 14:09:41 -0500

    set up client and server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,64 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+
+    //event listeners
+    $('#addJokeButton').on('click',addJoke);
+    
+}
+function getJokes() {
+    //requesting jokes data from the server
+    $.ajax({
+        method: 'GET',
+        url: '/jokes'
+    }).then(function(response){//res holds whatever got sent from server
+        console.log('success', response)
+        renderToDom(response);
+        
+    }).catch(function(error){ 
+        alert('request failed!'); 
+        console.log('request failed:', error)
+    })
+   
+
+}
+function renderToDom(jokesArray){
+    $('#jokeList').empty();
+    for (joke of jokesArray)
+    $('#jokeList').append(`<div>Joker:${joke.whoseJoke} Joke Question: ${jokeQuestion} Punchline:${punchLine}</div>
+    `)
+
+}
+
+function addJoke (event){
+    event.preventDefault();
+    //add inputs
+    const joker = $('#whoseJokeIn').val();
+    const jokeQuestion = $('#questionIn').val();
+    const punchLine = $('#punchlineIn').val();
+
+    //clear inputs
+    $('#whoseJokeIn').val('');
+    $('#questionIn').val('');
+    $('#punchlineIn').val('');
+
+    $.ajax({
+        method: 'POST',
+        url: '/jokes',
+        data: {
+            whoseJoke: whoseJoke,
+            jokeQuestion: jokeQuestion,
+            punchLine: punchLine
+
+        }
+    }).then(function(response){
+        console.log('success');
+        getJokes()
+        
+    }).catch(function(error){
+        alert('error with cakes post');
+        console.log('error with post', error);
+    
+    }) 
+
 }
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let jokes = [
+let jokesArray = [
   {
     whoseJoke: "Danny",
     jokeQuestion: "Why do scuba divers fall backwards out of boats?",
@@ -33,9 +33,26 @@ let jokes = [
     punchLine: "It was a shih tzu."
   }
 ];
-
 // serve back static files
 app.use(express.static('server/public'));
+
+//get routes
+
+app.get('/jokes', function(req,res){
+  console.log('request for jokes was made');
+  res.send(jokesArray); //send respond to the client
+})
+//post routes
+app.post('/jokes', function(req, res){
+  //req.body is data client sends in its request
+  //its from body parser 
+  console.log('post my data', req.body);
+  jokesArray.push(req.body); //will allow us to push new jokes in
+  res.sendStatus(201) 
+})
+
+
+
 
 app.listen(PORT, () => {
   console.log('server running on: ', PORT);
